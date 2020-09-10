2afc3a2da731c388c44c09dbb884d95ea0111137
mrkcancode <motasimkazmi333@gmail.com>
2020-09-11 03:37:51 +0530

    added UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import { fetchPosts } from '../actions/posts';
+import { PostsList } from './';
 
 class App extends React.Component {
   componentDidMount() {
@@ -9,8 +11,65 @@ class App extends React.Component {
   }
 
   render() {
-    console.log('Props', this.props);
-    return <div>App</div>;
+    const { posts } = this.props;
+    return (
+      <div>
+        <nav className="nav">
+          <div className="left-div">
+            <img
+              src="https://www.flaticon.com/svg/static/icons/svg/711/711284.svg"
+              alt="logo"
+            />
+          </div>
+          <div className="search-container">
+            <img
+              className="search-icon"
+              src="https://www.flaticon.com/svg/static/icons/svg/1007/1007692.svg"
+              alt="search-icon"
+            />
+            <input placeholder="Search" />
+            <div className="search-results">
+              <ul>
+                <li className="search-results-row">
+                  <img
+                    src="https://www.flaticon.com/svg/static/icons/svg/560/560216.svg"
+                    alt="user avatar"
+                  />
+                  <span>John Doe</span>
+                </li>
+                <li className="search-results-row">
+                  <img
+                    src="https://www.flaticon.com/svg/static/icons/svg/560/560216.svg"
+                    alt="user avatar"
+                  />
+                  <span>John Doe</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="right-nav">
+            <div className="user">
+              <img
+                src="https://www.flaticon.com/svg/static/icons/svg/560/560216.svg"
+                alt="user-dp"
+                id="user-dp"
+              />
+              <span>John Doe</span>
+            </div>
+            <div className="nav-links">
+              <ul>
+                <li>Log In</li>
+
+                <li>Log Out</li>
+
+                <li>Register</li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <PostsList posts={posts} />
+      </div>
+    );
   }
 }
 
@@ -19,5 +78,9 @@ function mapStateToProps(state) {
     posts: state.posts,
   };
 }
+
+App.propTypes = {
+  posts: PropTypes.array.isRequired,
+};
 
 export default connect(mapStateToProps)(App);
---
+++ src/components/PostsList.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class PostsList extends Component {
+  render() {
+    const { posts } = this.props;
+    return (
+      <div className="posts-list">
+        {posts.map((post) => (
+          <div className="post-wrapper" key={post._id}>
+            <div className="post-header">
+              <div className="post-avatar">
+                <img
+                  src="https://www.flaticon.com/svg/static/icons/svg/2922/2922506.svg"
+                  alt=""
+                />
+                <div>
+                  <span className="post-author">{post.user.name}</span>
+                  <span className="post-time">a minute ago</span>
+                </div>
+              </div>
+
+              <div className="post-content">{post.content}</div>
+              <div className="post-actions">
+                <div className="post-like">
+                  <img
+                    src="https://www.flaticon.com/svg/static/icons/svg/686/686308.svg"
+                    alt="like icon"
+                  />
+                  <span>{post.likes.length}</span>
+                </div>
+
+                <div className="post-comments-icon">
+                  <img
+                    src="https://www.flaticon.com/svg/static/icons/svg/907/907820.svg"
+                    alt="comment icon"
+                  />
+                  <span>{post.comments.length}</span>
+                </div>
+              </div>
+              <div className="post-comment-box">
+                <input placeholder="Start typing a comment"></input>
+              </div>
+              <div className="post-comments-list">
+                <div className="post-comments-item">
+                  <div className="post-comment-header">
+                    <span className="post-comment-author">John</span>
+                    <span className="post-comment-time">a minute ago</span>
+                    <span className="post-comment-likes">22</span>
+                  </div>
+
+                  <div className="post-comment-content">Random comment</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+PostsList.propTypes = {
+  posts: PropTypes.array.isRequired,
+};
+
+export default PostsList;
