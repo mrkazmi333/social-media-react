4a044dc5e23f0459e6d7f73a4aa0499d3412328f
mrkcancode <motasimkazmi333@gmail.com>
2020-09-15 19:31:49 +0530

    chat functionality added

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import io from 'socket.io-client';
 import '../chat.css';
+import { connect } from 'react-redux';
 
 class Chat extends Component {
   constructor(props) {
@@ -8,7 +10,58 @@ class Chat extends Component {
       messages: [], //{content: 'some message', self: true}
       typedMessage: '',
     };
+    this.socket = io.connect('http://54.237.158.65:5000');
+    this.userEmail = props.user.email;
+    // console.log('CHAT PROPS', props);
+
+    if (this.userEmail) {
+      this.setupConnections();
+    }
   }
+  setupConnections = () => {
+    const socketConnection = this.socket; // making this.socket as socketConnection because this here is the instance of chat class component but in the callbabck fn this is differet so it'll give error
+    const self = this;
+
+    this.socket.on('connect', function () {
+      console.log('HEY CONNECTION ESTABLSIHDED');
+      socketConnection.emit('join_room', {
+        user_email: this.userEmail,
+        chatroom: 'codeial',
+      });
+
+      socketConnection.on('user_joined', function (data) {
+        console.log('NEW USER JOINED', data);
+      });
+    });
+
+    this.socket.on('receive_message', function (data) {
+      //add message to state
+      const { messages } = self.state;
+      const messageObject = {};
+      messageObject.content = data.message;
+
+      if (data.user_email === self.userEmail) {
+        messageObject.self = true;
+      }
+
+      self.setState({
+        messages: [...messages, messageObject],
+        typedMessage: '',
+      });
+    });
+  };
+
+  handleSubmit = () => {
+    const { typedMessage } = this.state;
+
+    if (typedMessage && this.userEmail) {
+      this.socket.emit('send_message', {
+        message: typedMessage,
+        user_email: this.userEmail,
+        chatroom: 'codeial',
+      });
+    }
+  };
 
   render() {
     const { typedMessage, messages } = this.state;
@@ -26,7 +79,7 @@ class Chat extends Component {
           {messages.map((message) => (
             <div
               className={
-                messages.self
+                message.self
                   ? 'chat-bubble self-chat'
                   : 'chat-bubble other-chat'
               }
@@ -49,4 +102,10 @@ class Chat extends Component {
   }
 }
 
-export default Chat;
+function mapStateToProps({ auth }) {
+  return {
+    user: auth.user,
+  };
+}
+
+export default connect(mapStateToProps)(Chat);
